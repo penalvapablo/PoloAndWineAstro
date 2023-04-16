9669994582a8810b5ee20d8d11a011100e934215
Pablo Penalva <penalva.pablo@gmail.com>
2023-04-16 11:44:20 -0300

    herotext and whatsapp

--- src/components/Whatsapp.jsx
+++ src/components/Whatsapp.jsx
@@ -12,7 +12,7 @@ const Whatsapp = () => {
       {' '}
       <FontAwesomeIcon
         icon={faWhatsapp}
-        className="fixed bottom-32 right-0 z-40 h-10  rounded-bl-xl rounded-tl-xl bg-one p-3 text-two duration-100 hover:bg-two hover:text-one"
+        className="xl fixed bottom-16 right-0 z-40  h-10 rounded-bl-xl rounded-tl-xl bg-one p-3 text-two duration-100 hover:bg-two hover:text-one xsm:bottom-32"
       />
     </a>
   );
