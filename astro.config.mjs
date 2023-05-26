6b926501f2e43fc1700f17388998d6bfded2e02f
Pablo Penalva <penalva.pablo@gmail.com>
2023-05-26 11:13:08 -0300

    astro config vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,15 @@
 import { defineConfig } from 'astro/config';
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
-import image from "@astrojs/image";
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
+import image from '@astrojs/image';
 
-import vercel from "@astrojs/vercel/serverless";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), image()],
-  output: "server",
-  adapter: vercel()
+  output: 'server',
+  adapter: vercel({
+    analytics: true,
+  }),
 });
